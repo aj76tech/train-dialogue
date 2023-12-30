c20cc8a9c84f26fb8d6422409cb5b97bbd7be420
aj76tech <147583853+aj76tech@users.noreply.github.com>
2023-12-30 12:07:32 +0530

    Update train_custom_dialogue_model.py

--- train_custom_dialogue_model.py
+++ train_custom_dialogue_model.py
@@ -1,29 +1,29 @@
-from transformers import GPT2LMHeadModel, GPT2Tokenizer, GPT2LMHeadModel, GPT2Config
-from transformers import TextDataset, DataCollatorForLanguageModeling
-from transformers import Trainer, TrainingArguments
+from transformers import GPT2LMHeadModel, GPT2Tokenizer, TextDataset, DataCollatorForLanguageModeling, Trainer, TrainingArguments
 import torch
 import requests
+import os
 
 # Load DialoGPT-medium model and tokenizer
 model_name = "microsoft/DialoGPT-medium"
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 model = GPT2LMHeadModel.from_pretrained(model_name)
 
-# Load your custom dataset (replace 'path_to_your_dataset.txt' with your actual dataset path)
-
+# Load your custom dataset
 github_dataset_url = 'https://raw.githubusercontent.com/aj76tech/train-dialogue/main/dataset.txt'
 local_file_path = 'dataset.txt'
+
 # Download the file
 response = requests.get(github_dataset_url)
 with open(local_file_path, 'w', encoding='utf-8') as file:
     file.write(response.text)
+
 # Use the local file path in the TextDataset constructor
 dataset = TextDataset(
     tokenizer=tokenizer,
     file_path=local_file_path,
-    block_size=128,  # Adjust block_size as needed
+    block_size=128,
     overwrite_cache=True,
-    cache_dir='./cache'  # You can specify the cache directory
+    cache_dir='./cache'
 )
 
 # Define data collator
@@ -34,9 +34,9 @@ data_collator = DataCollatorForLanguageModeling(
 
 # Fine-tuning arguments
 training_args = TrainingArguments(
-    output_dir="./shopkeepr_model",  # Set the output directory
+    output_dir="./shopkeepr_model",
     overwrite_output_dir=True,
-    num_train_epochs=3,  # Adjust the number of training epochs
+    num_train_epochs=3,
     per_device_train_batch_size=4,
     save_steps=10_000,
     save_total_limit=2,
@@ -56,3 +56,15 @@ trainer.train()
 # Save the fine-tuned model
 model.save_pretrained("./shopkeepr_model")
 tokenizer.save_pretrained("./shopkeepr_model")
+
+# Display and download the saved model
+output_files = os.listdir("./shopkeepr_model")
+print("Files in the output directory:")
+print(output_files)
+
+# Download the model file (assuming there is only one .bin file)
+model_file = [file for file in output_files if file.endswith(".bin")][0]
+model_path = os.path.join("./shopkeepr_model", model_file)
+
+# Provide a link for downloading the model file
+print(f"\nDownload the trained model: [link]({model_path})")
